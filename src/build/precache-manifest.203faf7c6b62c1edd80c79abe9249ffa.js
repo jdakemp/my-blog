9ffee313a45525b760c58f9eab68705a3b9d8c14
JD Alexander Kemp <Jkemp@JDs-MacBook-Pro.local>
2020-01-04 13:26:49 -0500

    First Commit

---
+++ src/build/precache-manifest.203faf7c6b62c1edd80c79abe9249ffa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdd5feefc5de063b4d5ee8121a74d9fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "94fe2dd0d6364e6854e7",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "d4738422c3f9e5d79bf1",
+    "url": "/static/js/2.c08b00bf.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.c08b00bf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "94fe2dd0d6364e6854e7",
+    "url": "/static/js/main.5b6eead1.chunk.js"
+  },
+  {
+    "revision": "2829fc8889d8b91e8c4b",
+    "url": "/static/js/runtime-main.b519a9b9.js"
+  }
+]);
